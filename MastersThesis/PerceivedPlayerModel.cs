586963607ee7e03856a64cbe0f20c9a95356eb48
Alex Hoare <alexander_hoare@hotmail.co.uk>
2021-06-15 17:10:29 +0100

    Added new variables for health, traits and strategies.

--- MastersThesis/PerceivedPlayerModel.cs
+++ MastersThesis/PerceivedPlayerModel.cs
@@ -6,9 +6,11 @@ namespace MastersThesis
 {
     class PerceivedPlayerModel
     {
+        public int playerID;
         public int trust;
         public int threat;
         public int perceivedTrustfullness;
         public int perceivedDeceitfulness;
+
     }
 }
--- MastersThesis/Player.cs
+++ MastersThesis/Player.cs
@@ -7,8 +7,11 @@ namespace MastersThesis
     class Player
     {
         public int playerID;
+        public int health;
         public PlayerModel playerModel;
         public PerceivedPlayerModel perceivedPlayerModel;
+        public List<String> traits;
+        public List<String> strategies;
         public Player(int id)
         {
             playerID = id;
