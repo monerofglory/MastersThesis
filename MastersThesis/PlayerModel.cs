8cc98a497846a79c9b29d5327d22162521a7e550
Alex Hoare <alexander_hoare@hotmail.co.uk>
2021-06-16 09:09:38 +0100

    Added trust variable initialisation based on traits.

--- MastersThesis/PlayerModel.cs
+++ MastersThesis/PlayerModel.cs
@@ -6,8 +6,39 @@ namespace MastersThesis
 {
     class PlayerModel
     {
-        public int deceitfulness;
-        public int deceitAbility;
-        public int trust;
+        //Baseline variables are 50
+        public int deceitfulness = 50;
+        public int deceitAbility = 50;
+        public int trust = 50;
+        
+        public PlayerModel(Player p)
+        {
+            Random rd = new Random();
+            //Random trust variable
+            trust += rd.Next(-20, 20);
+            trust += TrustModifiers(p);
+        }
+
+        private int TrustModifiers(Player p)
+        {
+            int trustModifier = 0;
+            if (p.traits.Contains("Trusting"))
+            {
+                trust += 20;
+            }
+            if (p.traits.Contains("Untrusting"))
+            {
+                trust -= 20;
+            }
+            if (p.traits.Contains("Unsuspicious"))
+            {
+                trust += 10;
+            }
+            if (p.traits.Contains("Suspicious"))
+            {
+                trust -= 10;
+            }
+            return trustModifier;
+        }
     }
 }
